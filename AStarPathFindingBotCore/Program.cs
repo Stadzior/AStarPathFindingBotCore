ed4ce9a8f943c87fdf3c0fff261bf19b938a84d0
Stadzior <Stadzior@gmail.com>
2018-08-26 11:36:23 +0200

    Add project files.

---
+++ AStarPathFindingBotCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AStarPathFindingBotCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
